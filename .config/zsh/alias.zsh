6421d2992556dc11270fec8ad06a3e10fbc0c74a
Liam Paul <liam@pop-stack.org>
2021-06-25 22:05:00 -0700

    add another file i forgot to

---
+++ .config/zsh/alias.zsh
@@ -0,0 +1,13 @@
+alias la="ls -A1"
+alias ls="ls --color --group-directories-first"
+alias tmls="tmux ls"
+alias tmrn="tmux new-window ranger"
+alias cam="mpv av://v4l2:/dev/video0 --profile=low-latency --untimed"
+alias e=$EDITOR
+alias m="neomutt"
+alias vifm="vifmrun"
+alias sourcefunc="source ${HOME}/.config/zsh/functions"
+alias dmenu="dmenu -fn iosevka -nb #282828 -nf #d5c4a1 -sb #fabd2f -sf #ebdbb2"
+alias website="ssh -i ~/.local/share/ssh/id_rsa root@pop-stack.org"
+alias con="git --work-tree=$HOME --git-dir=$HOME/.git"
+alias fixmon="xrandr --output DVI-D-1 --off --output DP-1 --off --output DP-2 --off --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-3 --primary --mode 1920x1080 --pos 0x0 --rotate normal"
