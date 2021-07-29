5ddfaa03e9bf76aa60fe84df2a640db76882cb46
Liam Paul <liam@pop-stack.org>
2021-07-29 15:00:02 -0700

    add Neogit as g alias

--- alias.zsh
+++ alias.zsh
@@ -12,3 +12,4 @@ alias con="git --work-tree=$HOME --git-dir=$HOME/.git"
 alias fixmon="xrandr --output DVI-D-1 --off --output DP-1 --off --output DP-2 --off --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-3 --primary --mode 1920x1080 --pos 0x0 --rotate normal"
 alias bd="cd .."
 alias updatebar="pkill -47 dwmblocks"
+alias g='nvim +Neogit'
